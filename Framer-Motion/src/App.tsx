3185fab44f5dfef39a5c696e8365ba8bda4c1eb7
Susmita-Dey <susmitadey475@gmail.com>
2025-04-10 23:22:37 +0530

    Day1: Animate button using motion

---
+++ Framer-Motion/src/App.tsx
@@ -0,0 +1,12 @@
+// import "./App.css";
+import ButtonComponent from "./components/buttonComponent";
+
+function App() {
+  return (
+    <>
+      <ButtonComponent />
+    </>
+  );
+}
+
+export default App;
